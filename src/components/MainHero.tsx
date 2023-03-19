c71247fd600036ba276eb96dece1f6d3cb49104d
Kushagr Narayan <nkushagr@gmail.com>
2023-03-19 19:12:35 +0530

    second

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import config from '../config/index.json';
 
 const MainHero = () => {
   const { mainHero } = config;
+
+const [text, setText] = useState("")
 
   return (
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
@@ -18,24 +20,23 @@ const MainHero = () => {
           {mainHero.description}
         </p>
         <p className="mt-3 text-base text-secondary sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-custom leading-5 tracking-widest">
-          Launching Soon
+           {text}
         </p>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
-            <a
-              href={mainHero.primaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary  hover:text-border md:py-4 md:text-lg md:px-10`}
+            <div
+              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary  hover:text-border md:py-4 md:text-lg md:px-10 cursor-pointer`}
+              onClick={()=>setText("Coming Soon")}
             >
               {mainHero.primaryAction.text}
-            </a>
+            </div>
           </div>
           <div className="mt-3 sm:mt-0 sm:ml-3">
-            <a
-              href={mainHero.secondaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-background bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+            <div
+              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-background bg-border hover:text-primary md:py-4 md:text-lg md:px-10 cursor-pointer`} onClick={()=>setText("Coming Soon")}
             >
               {mainHero.secondaryAction.text}
-            </a>
+            </div>
           </div>
         </div>
       </div>
