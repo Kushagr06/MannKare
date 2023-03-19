8c0514e1db97f23501869751c886998468507756
Kushagr Narayan <nkushagr@gmail.com>
2023-03-19 19:12:09 +0530

    second

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,6 +7,7 @@ const About = () => {
   const { logo, name: companyName } = company;
   const { socialMedia, sections } = about;
 
+  const mailer = `mailto:${socialMedia.mail}`;
   return (
     <div
       id="about"
@@ -38,7 +39,7 @@ const About = () => {
             rel="noreferrer"
           >
             <svg
-              className="fill-current text-gray-200 dark:text-white hover:text-primary"
+              className="fill-current text-gray-200 dark:text-white"
               width="30px"
               height="30px"
               viewBox="0 0 24 24"
@@ -78,19 +79,14 @@ const About = () => {
           </a>
           <a
             aria-label="linkedin"
-            href={socialMedia.linkedin}
+            href={mailer}
             target="_blank"
             rel="noreferrer"
+            className="fill-current text-gray-200 dark:text-white hover:text-primary"
           >
-            <svg
-              className="fill-current text-gray-200 dark:text-white hover:text-primary"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-            </svg>
+           <svg 
+           
+           width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.272 6.365C2 6.9 2 7.6 2 9v6c0 1.4 0 2.1.272 2.635a2.5 2.5 0 0 0 1.093 1.092C3.9 19 4.6 19 6 19h12c1.4 0 2.1 0 2.635-.273a2.5 2.5 0 0 0 1.092-1.092C22 17.1 22 16.4 22 15V9c0-1.4 0-2.1-.273-2.635a2.5 2.5 0 0 0-1.092-1.093C20.1 5 19.4 5 18 5H6c-1.4 0-2.1 0-2.635.272a2.5 2.5 0 0 0-1.093 1.093zM4.647 7h14.706a.5.5 0 0 1 .278.916l-7.075 4.732a1 1 0 0 1-1.112 0L4.369 7.916A.5.5 0 0 1 4.647 7z" fill="#ffffff"></path></g></svg>
           </a>
         </div>
         <div className="flex items-center mt-6">
